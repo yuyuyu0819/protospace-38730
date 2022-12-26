56a2c61c5d37d646b8619cea26bb3374bcdfcd0f
yuyuyu0819 <safariabc.123@gmail.com>
2022-12-26 15:38:08 +0900

    修正２
    コメントの名前からユーザに正しく遷移しない問題

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -19,6 +19,7 @@ class PrototypesController < ApplicationController
   end
   
   def show
+  
     @prototype = Prototype.find(params[:id])
     @comment =Comment.new
     @comments = @prototype.comments
