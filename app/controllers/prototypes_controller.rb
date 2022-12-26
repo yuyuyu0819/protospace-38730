a0f0ed452bb5ed42c5a5f1fe9fa799d6592bacef
yuyuyu0819 <safariabc.123@gmail.com>
2022-12-26 09:05:32 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,57 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @prototypes=Prototype.all
+  end
+
+  def new 
+    @prototype=Prototype.new
+  end
+
+  def create
+    @prototype=Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment =Comment.new
+    @comments = @prototype.comments
+    
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    redirect_to root_path unless current_user.id == @prototype.user_id
+  end
+
+  def update
+ @prototype = Prototype.find(params[:id])
+ if @prototype.update(prototype_params)
+   redirect_to prototype_path(@prototype)
+    else
+      render :edit
+  end
+end
+
+def destroy
+ 
+  @prototype = Prototype.find(params[:id])
+  if @prototype.destroy
+    redirect_to root_path
+  else
+    redirect_to root_path
+  end
+end
+
+private
+def prototype_params
+  params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+end
+
+end
